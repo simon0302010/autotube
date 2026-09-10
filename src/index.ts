7446014b2b88233709200ef5ff9e15ba291f73af
simon0302010 <simon0302010@gmail.com>
2026-09-10 10:33:00 +0200

    Making a request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
 #!/usr/bin/env bun
 
+import OpenAI from "openai";
 import { askProvider } from "./models";
 
 const [apiSetup, models] = await askProvider();
+
+const client = new OpenAI({
+    apiKey: apiSetup.apiKey,
+    baseURL: apiSetup.baseUrl
+});
+
+const response = await client.responses.create({
+    model: "google/gemini-3.1-flash-lite",
+    instructions: "You are a helpful assistant.",
+    input: "Please tell me why Rust is better than Java."
+});
+
+console.log(response.output_text);
--- src/models.ts
+++ src/models.ts
@@ -14,7 +14,7 @@ interface ModelList {
 }
 
 interface ApiSetup {
-    endpoint: string,
+    baseUrl: string,
     apiKey: string
 }
 
@@ -22,17 +22,17 @@ export async function askProvider(): Promise<[ApiSetup, ModelList]> {
     const providers: readonly string[] = ["Hack Club AI", "OpenAI", "OpenRouter"];
     const provider = await select({message: "Please select your AI provider", choices: providers});
 
-    let endpoint: string;
+    let baseUrl: string;
 
     switch (provider) {
         case "Hack Club AI":
-            endpoint = "https://ai.hackclub.com/proxy/v1";
+            baseUrl = "https://ai.hackclub.com/proxy/v1";
             break;
         case "OpenAI":
-            endpoint = "https://api.openai.com/v1";
+            baseUrl = "https://api.openai.com/v1";
             break;
         case "OpenRouter":
-            endpoint = "https://openrouter.ai/api/v1";
+            baseUrl = "https://openrouter.ai/api/v1";
             break;
         default:
             console.error("Provider not found");
@@ -41,7 +41,7 @@ export async function askProvider(): Promise<[ApiSetup, ModelList]> {
 
     const apiKey = await password({message: "Please input your API key", toggleMask: true});
 
-    const response = await fetch(`${endpoint}/models`, {
+    const response = await fetch(`${baseUrl}/models`, {
         method: "GET",
         headers: {
             Accept: "application/json",
@@ -51,7 +51,7 @@ export async function askProvider(): Promise<[ApiSetup, ModelList]> {
     });
 
     if (response.ok) {
-        const apiSetup: ApiSetup = {endpoint, apiKey}
+        const apiSetup: ApiSetup = {baseUrl, apiKey}
         const models = await response.json() as ModelList;
 
         return [apiSetup, models];
